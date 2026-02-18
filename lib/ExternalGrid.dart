ba11e506fe5f705963bd3b86b9318fe95ed2e8f1
Corentin Jéré <corentinjere@gmail.com>
2026-02-18 23:27:38 +0100

    Create external grid game layout.

---
+++ lib/ExternalGrid.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Externalgrid extends StatelessWidget {
+  const Externalgrid({Key? key, required this.boxSize}) : super(key: key);
+
+  final boxSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(9, (boxIndex) {
+          int blocRow = boxIndex ~/ 3;
+          int blocCol = boxIndex % 3;
+          return Container(
+            width: boxSize,
+            height: boxSize,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueAccent)),
+          );
+        })
+    );
+  }
+}
