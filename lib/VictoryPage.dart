619a41e76b6b2da9523529b8aa281a2b1c7782e4
Corentin Jéré <corentinjere@gmail.com>
2026-02-27 11:36:26 +0100

    Add responsive design handling for the victory page.

--- lib/VictoryPage.dart
+++ lib/VictoryPage.dart
@@ -11,52 +11,55 @@ class VictoryPage extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Icon(
-                  Icons.emoji_events,
-                  size: 100,
-                  color: Colors.amber,
-                ),
-                const SizedBox(height: 24),
-                const Text(
-                  "Félicitations!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
+            child: SizedBox(
+              width: (MediaQuery.of(context).size.width/8*7).ceil().toDouble(),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(
+                    Icons.emoji_events,
+                    size: 100,
+                    color: Colors.amber,
                   ),
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  "Tu as réussi à compléter le sudoku.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(height: 40),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 40,
-                      vertical: 16,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
+                  const SizedBox(height: 24),
+                  const Text(
+                    "Félicitations!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  onPressed: () {
-                    context.go("/");
-                  },
-                  child: const Text(
-                    "Retour à l'acceuil",
-                    style: TextStyle(fontSize: 18),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Tu as réussi à compléter le sudoku.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
                   ),
-                ),
-              ],
-            ),
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 16,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {
+                      context.go("/");
+                    },
+                    child: const Text(
+                      "Retour à l'acceuil",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ),
         ),
       ),
